f2f91809a73be109c2e2da9ca3ac3aa005159998
Ankit <40290144+akharb1192@users.noreply.github.com>
2020-05-09 18:45:55 +0530

    Update database.sql

--- login-signup-form-master/database.sql
+++ login-signup-form-master/database.sql
@@ -26,3 +26,12 @@ INSERT INTO `quiz` (`id`, `que`, `option 1`, `option 2`, `option 3`, `option 4`,
 (3, ' ____________ function in PHP Returns a list of response headers sent (or ready to send)', 'header', 'headers_list', 'header_sent', 'header_send', 'headers_list', 'headers_list'),
 (4, 'Which of the following is the Server Side Scripting Language?', 'HTML', 'CSS', 'JS', 'PHP', 'PHP', 'PHP'),
 (5, 'From which website you download this source code?', 'Softglobe.net', 'w3school.com', 'technopoints.co.in', 'php.net', 'technopoints.co.in', 'technopoints.co.in');
+
+
+create table result 
+(
+username varchar(30),
+score int(10),
+description varchar(30),
+quizdate varchar(30)
+);
